21d922d79a95b66e581b032b2ced5af5878c48d9
Luis Rocha <luis.rocha.jacinto@gmail.com>
2017-01-31 21:59:37 +0000

    Identation fix

--- NodeJS/file_md5/main.js
+++ NodeJS/file_md5/main.js
@@ -1,12 +1,11 @@
 if (process.argv.length <= 2) {
-    console.log("You need to specify a file.");
-    process.exit(-1);
+	console.log("You need to specify a file.");
+	process.exit(-1);
 }
 
 var crypto = require('crypto');
 var fs = require('fs');
 var filename = process.argv[2]; 
-
 var md5_var = crypto.createHash('md5');
 
 
@@ -15,10 +14,10 @@ if (fs.existsSync(filename)) { // check if the file exists
 }
 
 my_file.on('data', function(d) {
-  md5_var.update(d);
+	md5_var.update(d);
 });
 
 my_file.on('end', function() {
-  var file_md5 = md5_var.digest('hex');
-  console.log('MD5 of file: ' + filename + '\n' + file_md5);
+	var file_md5 = md5_var.digest('hex');
+	console.log('MD5 of file: ' + filename + '\n' + file_md5);
 });
